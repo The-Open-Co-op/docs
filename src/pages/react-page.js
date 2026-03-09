ce1de566cdac3ee4a19a20b108db87ffd398eb41
olisb <osb@open.coop>
2026-03-09 19:40:54 +0000

    Create react-page.js

--- src/pages/react-page.js
+++ src/pages/react-page.js
@@ -5,7 +5,7 @@ export default function MyReactPage() {
   return (
     <Layout>
       <h1>My React page</h1>
-      <p>This is a React page test</p>
+      <p>This is a React page</p>
     </Layout>
   );
 }
